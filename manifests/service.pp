823417eff4bdc61d8a7b85dc91970f9b03cc9815
Marc Villacorta <marc.villacorta@gmail.com>
2014-09-24 19:19:13 +0200

    Fix service declaration order

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,7 +1,8 @@
 class nfs::service inherits nfs {
 
   service { 'nfs-server':
-    ensure => $ensure,
-    enable => $enable,
+    ensure  => $ensure,
+    enable  => $enable,
+    require => Package['nfs-utils'],
   }
 }
